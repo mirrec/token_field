ebe118ccaaa9d81cffb16c125a82e0c2c83009f5
Miroslav Hettes <mirrec@gmail.com>
2015-10-27 13:09:37 +0100

    Remove warning from simple form

--- lib/token_field/simple_form/token_input.rb
+++ lib/token_field/simple_form/token_input.rb
@@ -7,7 +7,11 @@ class TokenInput < SimpleForm::Inputs::Base
   #
   # many:
   # <%= f.input :category_ids, :as => :token %>
-  def input
-    @builder.token_field(attribute_name, options)
+  def input(wrapper_options=nil)
+    merged_input_options = input_options.merge(
+        merge_wrapper_options(input_html_options, wrapper_options)
+    )
+
+    @builder.token_field(attribute_name, merged_input_options)
   end
 end
